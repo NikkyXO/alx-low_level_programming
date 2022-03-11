dc99d708ddac38596cc55cef424836fd88892e03
NikkyXO <96661213+NikkyXO@users.noreply.github.com>
2022-03-11 14:40:27 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ if (n % 10 > 5)
 printf("and is greater than 5\n");
 if (n % 10 == 0)
 printf("and is 0\n");
-if (n % 10 < 6 && n % 10 != 10)
+if (n % 10 < 6 && n % 10 != 0)
 printf("and is less than 6 and not 0\n");
 return (0);
 }
