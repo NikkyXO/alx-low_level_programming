5bda0c0aecc91e7577624a040ab4cc9ca7945a4c
NikkyXO <96661213+NikkyXO@users.noreply.github.com>
2022-03-11 21:31:45 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -6,11 +6,11 @@
  */
 int main(void)
 {
-int n;
+      int n;
 
-for (n = 48; n < 58; n++)
-{
-putchar (n);
-}
-putchar('\n');
-return (0);
+      for (n = 48; n < 58; n++)
+      {
+            putchar (n);
+      }
+      putchar('\n');
+      return (0);
