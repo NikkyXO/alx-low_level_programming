b1fe6c788b0c4aa9caef2c65a777fbc9543a0c9f
NikkyXO <96661213+NikkyXO@users.noreply.github.com>
2022-03-17 17:21:19 +0100

    Update 4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,25 +1,19 @@
 #include "main.h"
 
 /**
- * print_most_numbers - prints numbers from number 0 - 9 followed by a new line
+ * print_most_numbers - prints numbers 0-9 except 2,4
  *
  * Return: void
- *
  */
 
-void print_most_numbers(void);
-
+void print_most_numbers(void)
 {
 	int c;
 
 	for (c = '0'; c <= '9'; c++)
-
 	{
 		if (c != '2' && c != '4')
-
-
 			_putchar(c);
 	}
-	putchar('\n');
-
+	_putchar('\n');
 }
