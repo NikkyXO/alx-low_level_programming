f719a339cdc6107d9011516341e413b99da944e8
NikkyXO <alexisdame2017@gmail.com>
2022-03-17 13:14:40 +0100

    More functions and loops

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+
 
 int _isupper(int c);
 int _isdigit(int c);
@@ -11,5 +10,3 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
-
-#endif
