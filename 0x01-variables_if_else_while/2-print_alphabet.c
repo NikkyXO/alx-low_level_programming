ebc5f57252e632c6be66ddb8a4c7f74f43c1ab3f
NikkyXO <alexisdame2017@gmail.com>
2022-03-17 15:04:43 +0100

    Low level programming

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,19 +2,19 @@
 
 /**
  * main - prints the alphabet in lowercase, and then in uppercase
- *
  * Return: Always 0 (Success)
  */
-
 int  main(void)
 {
-char alp[26] = "abcdefghijklmnopqrstuvwxyz";
-int i;
+        char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+        int i;
 
-for (i = 0; i < 26; i++)
-{
-putchar(alp[i]);
-}
-putchar('\n');
-return (0);
+        for (i = 0; i < 26; i++)
+
+        {
+        	putchar(alp[i]);
+        }
+
+        putchar('\n');
+        return (0);
 }
