0b921e9f07486ad4bf76cb76f2406fa62386a51b
NikkyXO <96661213+NikkyXO@users.noreply.github.com>
2022-03-18 01:11:36 +0100

    Update 1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - preventing an infinit loop from occurring
+ * main - Preventing an infinite loop from occuring
  *
  * Return: 0
  */
@@ -13,13 +13,12 @@ printf("Infinite loop incoming :(\n");
 i = 0;
 
 /*
- * while (i < 10)
- * {
- * infinite loop - No increase of variable
- * putchar(i);
- * }
- */
-
+*while (i < 10)
+*{
+*Infinite loop - No increase of variable
+*putchar(i);
+}
+*/
 printf("Infinite loop avoided! \\o/\n");
 return (0);
 }
