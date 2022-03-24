253a4249ff60a5886e35c55ae7abb0e0ca625472
NikkyXO <96661213+NikkyXO@users.noreply.github.com>
2022-03-24 16:17:59 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,14 +1,12 @@
 #include "main.h"
 
-int check_separators(char c);
+int check_seperators(char c);
 
 /**
- * cap_string -  function that capitalizes
- * all words of a string.
+ * cap_string - a function that capitalizes all words of a string.
  * @s: An input string to capitalize letters
  * Return: pointer to s
  */
-
 char *cap_string(char *s)
 {
 	int i = 0;
@@ -18,38 +16,29 @@ char *cap_string(char *s)
 		if (i == 0 && (s[i] >= 'a' && s[i] <= 'z'))
 			s[i] -= 32;
 
-		if (check_separators(s[i]) && (s[i + 1] >= 'a'
-					&& s[i + 1] <= 'z'))
+		if (check_seperators(s[i]) && (s[i + 1] >= 'a' && s[i + 1] <= 'z'))
 			s[i + 1] -= 32;
-			i++;
-
+		i++;
 	}
 
 	return (s);
-
-
 }
 
 /**
- * check_separators - separators of words: space, tabulation, new line, etc.
- * ,,;,.,!,?,",(,),{,} etc
+ * check_seperators - Separators of words: space, tabulation, new line,
+ * ,, ;, ., !, ?, ", (, ), {, and }
  * @c: an input character
- * Return: 1 if separator, 0 otherwise
+ * Return: 1 if seperator, 0 otherwise
  */
-
-int check_separators(char c)
+int check_seperators(char c)
 {
 	int i = 0;
-
-	char separators[13] = { ' ', '\t', '\n', ',', ':', '.', '!',
-		'?', '"', '(', ')', '{', '}', };
-
-        for (; i < 13; i++)
-        {
-		if (c == separators[i])
-
+	char seperators[13] = { ' ', '\t', '\n', ',', ';', '.', '!', '?',
+		'"', '(', ')', '{', '}' };
+	for (; i < 13; i++)
+	{
+		if (c == seperators[i])
 			return (1);
 	}
-
 	return (0);
 }
