f4f4cad4248d1217b627600f105c1534716944c2
NikkyXO <alexisdame2017@gmail.com>
2022-04-01 14:27:50 +0100

    A program that prints the minimum number of coins to make change for an amount of money.

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "stdlib.h"
+#include "stdio.h"
+
+/**
+  * make_change - finds least number of coins
+  * @n: money amount
+  * Return: number 0f coins
+  */
+
+int make_change(int n)
+{
+	
