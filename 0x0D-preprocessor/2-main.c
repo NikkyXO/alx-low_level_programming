f5001faf8c9b3f0c0ca83122d6435700b3932178
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-04-11 13:56:44 +0100

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
-
 /**
-  * main - prints the name of the file it was compiled
-  * from, followed by a new line
-  * Return: 0 if successful
-  *
-  */
-
-int  main(void)
+ * main - prints the name of file
+ * Return: Always 0 (Success)
+ */
+int main(void)
 {
-	printf("%s\n", _FILE_);
+	printf("%s\n", __FILE__);
 	return (0);
 }
