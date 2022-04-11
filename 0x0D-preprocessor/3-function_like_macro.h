c6e8faf688b5868ffbd32322cd2ab4ec1d12e80e
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-04-11 14:13:57 +0100

    Update 3-function_like_macro.h

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _FUNC_LIKE_MACRO_H
-#define _FUNC_LIKE_MACR0_H
-#define ABS(x) (((x) < 0) ? -(x) : (x))
-#endif
+#ifndef FUNC_LIKE_MACRO_H
+#define FUNC_LIKE_MACRO_H
+#define ABS(x)  ((x) > (-1) ? (x) : (x * -1))
+#endif /* !FUNC_LIKE_MACRO_H */
