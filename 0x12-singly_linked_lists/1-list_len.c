60d099ae221ee7c3bba0eed390a59fa616e9bccf
NikkyXO <alexisdame2017@gmail.com>
2022-04-21 13:41:11 +0100

    prints all the elements of list_t list

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,8 +3,8 @@
 /**
  * print_list - prints all the elements of list_t list
  * @h: head of linked list
- * 
- *  
+ *
+ *
  * Return: number of elements
  */
 
@@ -17,13 +17,12 @@ size_t print_list(const list_t *h)
 	{
 		size++;
 
-		if (h -> str == NULL)		
+		if (h->str == NULL)
 			printf("[%d] (nil)\n ", 0);
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		h = h->next;
 	}
 
-	return size;
-
+	return (size);
 }
