309653d557e6cbac4a81a9b03f9628eef33f5931
Olanike Ogundiran <96661213+NikkyXO@users.noreply.github.com>
2022-04-26 03:02:27 +0100

    frees a listint_t list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,19 +1,18 @@
 #include "lists.h"
 
 /**
- * free_listint2 - free a listint_t list
- *@head: the head of the list
- * Return: Nothing
+ * free_listint2 - frees a listint_t list
+ * @head: head of linked list
+ * Return: void
  */
 
 void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-
-	if (*head == NULL)
+	if (head == NULL)
 		return;
+	temp = *head;
 	while (temp != NULL)
 	{
 		temp = temp->next;
